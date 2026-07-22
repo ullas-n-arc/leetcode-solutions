067fd74ed517c16e96c865f8d14ce3ccf054ed7c
ULLAS N <ullas.np47@gmail.com>
2026-07-22 21:09:21 +0530

    Time: 3 ms (79.1%), Space: 43.8 MB (94.35%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<String> ans=new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        StringBuilder sb=new StringBuilder();
+        myFun(root,sb);
+        return ans;
+    }
+    public void  myFun(TreeNode root,StringBuilder sb){
+        // if(root==null) {
+        //     ans.add(sb.toString());
+        //     return;
+        // }
+        // sb.append(root.val);
+        // //int checkPoint=sb.length();
+        // myFun(root.left,sb);
+        // //sb.setLength(checkPoint);
+
+
+        // //checkPoint=sb.length();
+        // myFun(root.right,sb);
+        // //sb.setLength(checkPoint);
+        
+
+        // valid path only if both child are null
+        //sb.append(root.val);
+        if(root.left==null&&root.right==null){
+            sb.append(root.val);
+            ans.add(sb.toString());
+            return;
+        }
+        sb.append(root.val+"->");// not last
+        if(root.left==null){
+            myFun(root.right,sb);
+        }else if(root.right==null){
+            myFun(root.left,sb);
+        }else{
+            int checkPoint=sb.length();
+            myFun(root.left,sb);
+            sb.setLength(checkPoint);
+            myFun(root.right,sb);
+            sb.setLength(checkPoint);
+        }
+
+    }
+}
