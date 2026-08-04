1ccda946b4031de96cba6e9e6e20f2439445d07c
ULLAS N <ullas.np47@gmail.com>
2026-08-04 14:02:08 +0530

    Time: 15 ms (87.5%), Space: 69.8 MB (59.08%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -14,12 +14,7 @@ class Solution {
         }
         TreeNode left=lowestCommonAncestor(root.left,p,q);
         TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left==null){
-            return right;
-        }else if(right==null){
-            return left;
-        }else{
-            return root;//found
-        }
+        if(left!=null&&right!=null) return root;
+        return left==null?right:left;
     }
 }
