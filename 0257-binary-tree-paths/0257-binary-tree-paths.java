5b8608ec8824448794175b566af0ae9d2c5c3505
ULLAS N <ullas.np47@gmail.com>
2026-08-04 13:52:59 +0530

    Time: 3 ms (78.79%), Space: 43.7 MB (97.38%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -16,40 +16,24 @@
 class Solution {
     List<String> ans=new ArrayList<>();
     public List<String> binaryTreePaths(TreeNode root) {
-        StringBuilder sb=new StringBuilder();
-        myFun(root,sb);
+        myFun(root,new StringBuilder());
         return ans;
     }
     public void  myFun(TreeNode root,StringBuilder sb){
-        // if(root==null) {
-        //     ans.add(sb.toString());
-        //     return;
-        // }
-        // sb.append(root.val);
-        // //int checkPoint=sb.length();
-        // myFun(root.left,sb);
-        // //sb.setLength(checkPoint);
-
-
-        // //checkPoint=sb.length();
-        // myFun(root.right,sb);
-        // //sb.setLength(checkPoint);
-        
-
-        // valid path only if both child are null
-        //sb.append(root.val);
-        if(root==null) return;
         if(root.left==null&&root.right==null){
             sb.append(root.val);
             ans.add(sb.toString());
-            return;
         }
-        sb.append(root.val+"->");// not last
-            int checkPoint=sb.length();
+        sb.append(root.val+"->");
+        if(root.left!=null){
+            int len=sb.length();
             myFun(root.left,sb);
-            sb.setLength(checkPoint);
+            sb.setLength(len);
+        }
+        if(root.right!=null){
+            int len=sb.length();
             myFun(root.right,sb);
-            sb.setLength(checkPoint);
-
+            sb.setLength(len);
+        }
     }
 }
