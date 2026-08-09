d5279fc79282c7ded378e0abf2902ec3f1aed910
ULLAS N <ullas.np47@gmail.com>
2026-08-09 19:58:11 +0530

    Time: 10 ms (29.99%), Space: 55.6 MB (72.83%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,43 +1,35 @@
 class Solution {
-    class Pair{
-        int row;
-        int col;
-        Pair(int row,int col){
-            this.row=row;
-            this.col=col;
-        }
-    }
     public int numEnclaves(int[][] grid) {
-        Deque<Pair> q=new ArrayDeque<>();
-        boolean visited[][]=new boolean[grid.length][grid[0].length];
+        int[][] visited=new int[grid.length][grid[0].length];
+        Deque<int[]> q=new ArrayDeque<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
                 if(i==0||i==grid.length-1||j==0||j==grid[i].length-1){
-                    if(grid[i][j]==1){
-                        q.offerLast(new Pair(i,j));
-                        visited[i][j]=true;
-                    }
+                    if(grid[i][j]==1)
+                    q.offerLast(new int[]{i,j});
+                    visited[i][j]=1;
                 }
             }
         }
         while(!q.isEmpty()){
-            Pair p=q.pollFirst();
-            int row=p.row;
-            int col=p.col;
-            int[][] dir={{1,0},{-1,0},{0,1},{0,-1}};
-            for(int d[]:dir){
-                int newRow=row+d[0];
-                int newCol=col+d[1];
-                if(newRow>=0&&newRow<grid.length&&newCol>=0&&newCol<grid[0].length&&visited[newRow][newCol]==false&&grid[newRow][newCol]==1){
-                    visited[newRow][newCol]=true;
-                    q.offerLast(new Pair(newRow,newCol));
+            int[] arr=q.pollFirst();
+            int row=arr[0];
+            int col=arr[1];
+            int dr[]={-1,1,0,0};
+            int dc[]={0,0,-1,1};
+            for(int i=0;i<4;i++){
+                int r=row+dr[i];
+                int c=col+dc[i];
+                if(r>=0&&r<grid.length&&c>=0&&c<grid[r].length&&grid[r][c]==1&&visited[r][c]!=1){
+                    visited[r][c]=1;
+                    q.offerLast(new int[]{r,c});
                 }
             }
         }
         int count=0;
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j]==1&&visited[i][j]==false){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1&&visited[i][j]!=1){
                     count++;
                 }
             }
