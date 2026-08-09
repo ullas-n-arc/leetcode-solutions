26c18364a90fce421b14d34efd62096faaa73e64
ULLAS N <ullas.np47@gmail.com>
2026-08-09 12:13:39 +0530

    Time: 2 ms (40.98%), Space: 50 MB (6.82%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,29 +1,29 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int v=isConnected.length;
-        ArrayList<Integer>[] adj=new ArrayList[v];
-        for(int i=0;i<v;i++){
+        int n=isConnected.length;
+        ArrayList<Integer>[] adj=new ArrayList[n];
+        for(int i=0;i<n;i++){
             adj[i]=new ArrayList<>();
         }
-        for(int i=0;i<v;i++){
-            for(int j=i+1;j<v;j++){
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
                 if(isConnected[i][j]==1){
                     adj[i].add(j);
                     adj[j].add(i);
                 }
             }
         }
-        int count=0;
-        boolean visited[]=new boolean[v];
-        for(int i=0;i<v;i++){
+        int components=0;
+        boolean visited[]=new boolean[n];
+        for(int i=0;i<n;i++){
             if(!visited[i]){
-                count++;
-                bfs(i,adj,visited);
+                components++;
+                bfs(adj,i,visited);
             }
         }
-        return count;
+        return components;
     }
-    void bfs(int node,ArrayList<Integer>[] adj,boolean[] visited){
+    void bfs(ArrayList<Integer>[] adj,int node,boolean[] visited){
         Deque<Integer> q=new ArrayDeque<>();
         q.offerLast(node);
         visited[node]=true;
