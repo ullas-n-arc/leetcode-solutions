b5969fbca28b7cba9a4b7b376c1cb6101c3e6eae
ULLAS N <ullas.np47@gmail.com>
2026-08-11 23:47:18 +0530

    Time: 5 ms (63.99%), Space: 50.1 MB (64.87%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> ans=new ArrayList<>();
+        int currentNumber=1;
+        for(int i=0;i<n;i++){
+            ans.add(currentNumber);
+            if(currentNumber*10<=n){
+                currentNumber*=10;
+            }else{
+                while(currentNumber+1>n||currentNumber%10==9){
+                    currentNumber/=10;
+                }
+                currentNumber++;
+            }
+        }
+        return ans;
+    }
+}
