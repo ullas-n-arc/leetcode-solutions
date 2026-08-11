7b2f15f22161e485d6b24a91fe9ecb07783c4a55
ULLAS N <ullas.np47@gmail.com>
2026-08-11 19:19:37 +0530

    Time: 0 ms (100%), Space: 43.4 MB (60.84%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left=0;
+        int mid=0;
+        int right=nums.length-1;
+        //0..left all 0's
+        //left to mid //processing
+        //mid to right//1,s
+        //right to end//2's
+        while(mid<=right){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[left];
+                nums[left]=temp;
+                left++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp=nums[mid];
+                nums[mid]=nums[right];
+                nums[right]=temp;
+                right--;
+            }
+        }
+    }
+}
