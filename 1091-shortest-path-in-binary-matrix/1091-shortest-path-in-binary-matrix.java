a8abdc0fec14c3ae8d1ccadfe9fbf24bb8b4db42
ULLAS N <ullas.np47@gmail.com>
2026-08-30 13:43:59 +0530

    Time: 43 ms (5.17%), Space: 47.4 MB (80.34%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,36 +2,49 @@ class Solution {
     class Pair{
         int row;
         int col;
-        int dis;
+        int dist;
         Pair(int row,int col,int dis){
             this.row=row;
             this.col=col;
-            this.dis=dis;
+            this.dist=dis;
         }
     }
     public int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
         if(grid[0][0]==1) return -1;
-        Deque<Pair> q=new ArrayDeque<>();
-        boolean[][] visited=new boolean[grid.length][grid[0].length];
-        visited[0][0]=true;
-        q.offerLast(new Pair(0,0,0));
-        while(!q.isEmpty()){
-            Pair p=q.pollFirst();
-            int row=p.row;
-            int col=p.col;
-            int dist=p.dis;
-            if(row==grid.length-1&&col==grid[0].length-1) return dist+1;
-            int dir[][]={{-1,0},{1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
-            for(int d[]:dir){
-                int r=row+d[0];
-                int c=col+d[1];
-                if(isValid(r,c,grid.length,grid[0].length)&&!visited[r][c]&&grid[r][c]==0){
-                    visited[r][c]=true;
-                    q.offerLast(new Pair(r,c,dist+1));
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->{
+            if(a.dist==b.dist){
+                Integer.compare(a.row,b.row);
+            }
+            return Integer.compare(a.dist,b.dist);
+        }
+        );
+        pq.offer(new Pair(0,0,0));
+        int[][] dist=new int[n][m];
+        for(int row[]:dist){
+            Arrays.fill(row,Integer.MAX_VALUE);
+        }
+        dist[0][0]=0;
+        while(!pq.isEmpty()){
+            Pair pair=pq.poll();
+            int row=pair.row;
+            int col=pair.col;
+            int curDis=pair.dist;
+            int[][] dirs={{-1,0},{1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,1},{1,-1}};
+            for(int dir[]:dirs){
+                int newRow=row+dir[0];
+                int newCol=col+dir[1];
+                if(isValid(newRow,newCol,n,m)&&grid[newRow][newCol]==0&&1+curDis<dist[newRow][newCol]){
+                    dist[newRow][newCol]=curDis+1;
+                    pq.offer(new Pair(newRow,newCol,curDis+1));
                 }
             }
         }
-        return -1;
+        if(dist[n-1][m-1]==Integer.MAX_VALUE){
+            return -1;
+        }
+        return dist[n-1][m-1]+1;
     }
     boolean isValid(int r,int c,int R,int C){
         if(r<0||c<0||r>=R||c>=C){
