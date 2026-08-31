4478f7d61defb630e0b4df5ca9217e704727c7fb
ULLAS N <ullas.np47@gmail.com>
2026-08-31 22:43:52 +0530

    Time: 4 ms (99.87%), Space: 106.1 MB (69.98%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -11,31 +11,32 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         //i will find positions of all critical point
-        ArrayList<Integer> points=new ArrayList<>();
         int i=1;
         ListNode prev=head;
         ListNode cur=head.next;
         ListNode next=cur.next;
+        int firstSeenPoint=0;
+        int lastSeenPoint=0;
+        int curSeenPoint=0;
+        int maxDistance=-1;
+        int minDistance=Integer.MAX_VALUE;
         while(next!=null){
             if(isCritical(prev.val,cur.val,next.val)){
-                points.add(i);
+                curSeenPoint=i;
+                if(firstSeenPoint==0){
+                    firstSeenPoint=i;
+                }else{
+                    minDistance=Math.min(minDistance,curSeenPoint-lastSeenPoint);
+                }
+                lastSeenPoint=i;
             }
             i++;
             prev=cur;
             cur=next;
             next=next.next;
         }
-        if(points.size()==0||points.size()==1) return new int[]{-1,-1};
-        //max diff will be two extreme points 
-        //min distance i have to traverse points
-        int maxDistance=points.get(points.size()-1)-points.get(0);
-        int minDistance=maxDistance;
-        for(i=1;i<points.size();i++){
-            int diff=points.get(i)-points.get(i-1);
-            if(diff<minDistance){
-                minDistance=diff;
-            }
-        }
+        if(firstSeenPoint==0||firstSeenPoint==lastSeenPoint) return new int[]{-1,-1};
+        maxDistance=lastSeenPoint-firstSeenPoint;
         return new int[]{minDistance,maxDistance};
     }
     boolean isCritical(int prev,int cur,int next){
