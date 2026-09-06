e9eee932705e544c7130dca453031818a857f6f1
ULLAS N <ullas.np47@gmail.com>
2026-09-06 23:19:30 +0530

    Time: 20 ms (7.34%), Space: 50.7 MB (5.33%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,33 +1,48 @@
 class Solution {
+    class Pair{
+        int row;
+        int col;
+        int distance;
+        Pair(int row,int col,int distance){
+            this.row=row;
+            this.col=col;
+            this.distance=distance;
+        }
+    }
     public int[][] updateMatrix(int[][] mat) {
-        //multi source bfs expand from 0
-        Deque<int[]> q=new ArrayDeque<>();
-        boolean[][] visited=new boolean[mat.length][mat[0].length];
-        int distance[][]=new int[mat.length][mat[0].length];
+        int r=mat.length;
+        int c=mat[0].length;
+        int distance[][]=new int[r][c];
+        boolean[][] visited=new boolean[r][c];
+        Deque<Pair> q=new ArrayDeque<>();
         for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
+            for(int j=0;j<mat[0].length;j++){
                 if(mat[i][j]==0){
-                    q.offerLast(new int[]{i,j,0});
+                    q.offerLast(new Pair(i,j,0));
                     visited[i][j]=true;
                 }
             }
         }
         while(!q.isEmpty()){
-            int[] arr=q.pollFirst();
-            int r=arr[0];
-            int c=arr[1];
-            int d=arr[2];
-            int[][] dir={{-1,0},{1,0},{0,-1},{0,1}};
-            for(int di[]:dir){
-                int row=r+di[0];
-                int col=c+di[1];
-                if(row>=0&&row<mat.length&&col>=0&&col<mat[0].length&&visited[row][col]==false){
-                    distance[row][col]=d+1;
-                    q.offerLast(new int[]{row,col,d+1});
-                    visited[row][col]=true;
-                }
+           Pair p=q.pollFirst();
+           int curRow=p.row;
+           int curCol=p.col;
+           int curDistance=p.distance;
+           int dirs[][]={{-1,0},{1,0},{0,1},{0,-1}};
+           for(int dir[]:dirs){
+            int newRow=curRow+dir[0];
+            int newCol=curCol+dir[1];
+            if(isValid(newRow,newCol,r,c)&&!visited[newRow][newCol]){
+                visited[newRow][newCol]=true;
+                distance[newRow][newCol]=curDistance+1;
+                q.offerLast(new Pair(newRow,newCol,curDistance+1));
             }
+           }
         }
         return distance;
-    }  
+    }
+    boolean isValid(int r,int c,int R,int C){
+        if(r<0||r>=R||c<0||c>=C) return false;
+        return true;
+    }
 }
