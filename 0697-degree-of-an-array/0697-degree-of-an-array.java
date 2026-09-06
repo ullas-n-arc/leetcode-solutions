0a20a92cb367a9ec3bb13f35ceccf5f51bd1e6c5
ULLAS N <ullas.np47@gmail.com>
2026-09-06 19:51:00 +0530

    Time: 12 ms (93.06%), Space: 51.3 MB (8.81%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,28 +1,36 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        HashMap<Integer,Integer> firstSeen=new HashMap<>();
-        HashMap<Integer,Integer> lastSeen=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            firstSeen.putIfAbsent(nums[i],i);
-            lastSeen.put(nums[i],i);
+        int[] freq = new int[50000];
+        int[] firstSeen = new int[50000];
+        int[] lastSeen = new int[50000];
+
+        Arrays.fill(firstSeen, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            freq[num]++;
+
+            if (firstSeen[num] == -1) {
+                firstSeen[num] = i;
+            }
+
+            lastSeen[num] = i;
         }
-        int degree=-1;
-        int ans=Integer.MAX_VALUE; 
-        ArrayList<Integer> elements=new ArrayList<>();   
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            int value=entry.getValue();
-            if(value>degree){
+        int degree = -1;
+        int ans = Integer.MAX_VALUE;
+        ArrayList<Integer> elements = new ArrayList<>();
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>degree){
                 elements.clear();
-                degree=value;
-                elements.add(entry.getKey());
-            }else if(value==degree){
-                elements.add(entry.getKey());
+                degree=freq[i];
+                elements.add(i);
+            }else if(freq[i]==degree){
+                elements.add(i);
             }
         }
-        for(int ele:elements){
-            ans=Math.min(ans,lastSeen.get(ele)-firstSeen.get(ele)+1);
+        for (int ele : elements) {
+            ans = Math.min(ans, lastSeen[ele] - firstSeen[ele]+1);
         }
         return ans;
     }
