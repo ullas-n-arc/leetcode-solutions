73dcd2b50bd00a0d803a7c1e4de70ca97b276260
ULLAS N <ullas.np47@gmail.com>
2026-09-07 08:33:45 +0530

    Time: 2 ms (88.7%), Space: 44.9 MB (5.74%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,45 +3,58 @@ class Solution {
         int row;
         int col;
         int time;
-        Pair(int row_,int col_,int time_){
-            row=row_;
-            col=col_;
-            time=time_;
+        Pair(int row,int col,int time){
+            this.row=row;
+            this.col=col;
+            this.time=time;
         }
     }
     public int orangesRotting(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int time=0;
         Deque<Pair> q=new ArrayDeque<>();
-        int t=0;
+        boolean[][] visited=new boolean[n][m];
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
                     q.offerLast(new Pair(i,j,0));
+                    visited[i][j]=true;
                 }
             }
         }
         while(!q.isEmpty()){
             Pair p=q.pollFirst();
-            int r=p.row;
-            int c=p.col;
-            int time=p.time;
-            int dir[][]={{-1,0},{1,0},{0,-1},{0,1}};
-            for(int d[]:dir){
-                int row=r+d[0];
-                int col=c+d[1];
-                if(row>=0&&row<grid.length&&col>=0&&col<grid[row].length&&grid[row][col]==1){
-                    grid[row][col]=2;
-                    q.offerLast(new Pair(row,col,time+1));
-                    t=Math.max(t,time+1);
+            int curRow=p.row;
+            int curCol=p.col;
+            int curTime=p.time;
+            int dirs[][]={
+                {-1,0},
+                {1,0},
+                {0,1},
+                {0,-1}
+            };
+            for(int dir[]:dirs){
+                int newRow=curRow+dir[0];
+                int newCol=curCol+dir[1];
+                if(isValid(newRow,newCol,n,m)&&!visited[newRow][newCol]&&grid[newRow][newCol]==1){
+                    visited[newRow][newCol]=true;
+                    q.offerLast(new Pair(newRow,newCol,curTime+1));
                 }
             }
+            time=Math.max(time,curTime);
         }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j]==1){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1&&visited[i][j]==false){
                     return -1;
                 }
             }
         }
-        return t;
+        return time;
+    }
+    boolean isValid(int r,int c,int R,int C){
+        if(r<0||c<0||r>=R||c>=C) return false;
+        return true;
     }
 }
