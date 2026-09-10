226bee12a3eabd0e46575cdcd0d7a3d6720d5f27
ULLAS N <ullas.np47@gmail.com>
2026-09-10 12:35:47 +0530

    Time: 0 ms (100%), Space: 45.5 MB (48.87%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -14,31 +14,22 @@
  * }
  */
 class Solution {
+    int ans=0;
     public int averageOfSubtree(TreeNode root) {
-        return myFun(root);
-    }
-    int myFun(TreeNode root){
-        if(root==null) return 0;
-        int[] leftSum=sumCount(root.left);
-        int[] rightSum=sumCount(root.right);
-        int sum=root.val+leftSum[0]+rightSum[0];
-        int count=1+leftSum[1]+rightSum[1];
-        if(count!=0){
-            if(sum/count==root.val){
-                return 1+myFun(root.left)+myFun(root.right);
-            }
-        }
-        return myFun(root.left)+myFun(root.right);
+        sumCount(root);
+        return ans;
     }
     int[] sumCount(TreeNode root){
         if(root==null){
             return new int[]{0,0};
         }
-        if(root.left==null&&root.right==null){
-            return new int[]{root.val,1};
-        }
         int right[]=sumCount(root.right);
         int left[]=sumCount(root.left);
-        return new int[]{root.val+left[0]+right[0],1+left[1]+right[1]};
+        int sum=root.val+left[0]+right[0];
+        int count=1+left[1]+right[1];
+        if(sum/count==root.val){
+            ans++;
+        }
+        return new int[]{sum,count};
     }
 }
