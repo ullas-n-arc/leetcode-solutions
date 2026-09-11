33b0d4ce9aef4d1e88db930ca7371a68cb78f611
ULLAS N <ullas.np47@gmail.com>
2026-09-11 23:20:14 +0530

    Time: 1 ms (100%), Space: 43.8 MB (93.07%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,20 +1,27 @@
 class Solution {
     public int totalNumbers(int[] digits) {
         int n=digits.length;
-        Set<Integer> mySet=new HashSet<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
-                    if(i==j||i==k||j==k){
-                        continue;
-                    }
-                    int num=(digits[i]*100)+(digits[j]*10)+(digits[k]*1);
-                    if(num>=100&&num%2==0){
-                        mySet.add(num);
-                    }
-                }
-            }
+        int[] map=new int[10];
+        int count=0;
+        for(int num:digits){
+            map[num]++;
         }
-        return mySet.size();
+        for(int i=1;i<=9;i++){
+            if(map[i]==0) continue;
+            map[i]--;
+            for(int j=0;j<=9;j++){
+                if(map[j]==0) continue;
+                map[j]--;
+                for(int k=0;k<=8;k+=2){
+                    if(map[k]==0) continue;
+                    map[k]--;
+                    count++;
+                    map[k]++;
+                }
+                map[j]++;
+            }
+            map[i]++;
+        }
+        return count;
     }
 }
