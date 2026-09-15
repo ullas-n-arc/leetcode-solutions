1604301843bc412141e3d90ba73ca50e5bc98c45
ULLAS N <ullas.np47@gmail.com>
2026-09-15 22:27:17 +0530

    Time: 93 ms (8.15%), Space: 47.2 MB (74.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,52 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        ArrayList<Integer>[] adj=new ArrayList[n];
-        for(int i=0;i<n;i++){
-            adj[i]=new ArrayList<>();
+    class DSU{
+        int[] rank,parent;
+        int n;
+        DSU(int n){
+            this.n=n;
+            rank=new int[n];
+            parent=new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+            }
         }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+        int findRepresentative(int n){
+            if(parent[n]==n){
+                return n;
+            }
+            return parent[n]=findRepresentative(parent[n]);
+        }
+        void unionByRank(int u,int v){
+            int uPU=findRepresentative(u);
+            int uPV=findRepresentative(v);
+            if(uPU==uPV) return;
+            if(rank[uPU]<rank[uPV]){
+                parent[uPU]=uPV;
+            }else if(rank[uPV]<rank[uPU]){
+                parent[uPV]=uPU;
+            }else{
+                parent[uPV]=uPU;
+                rank[uPU]++;
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        DSU dsu=new DSU(isConnected.length);
+        int components=isConnected.length;
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=i+1;j<isConnected[0].length;j++){
                 if(isConnected[i][j]==1){
-                    adj[i].add(j);
-                    adj[j].add(i);
+                    int u=i;
+                    int v=j;
+                    int uPU=dsu.findRepresentative(u);
+                    int uPV=dsu.findRepresentative(v);
+                    if(uPU!=uPV){
+                        components--;
+                        dsu.unionByRank(u,v);
+                    }
                 }
             }
         }
-        int components=0;
-        boolean visited[]=new boolean[n];
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                components++;
-                dfs(adj,i,visited);
-            }
-        }
         return components;
-    }
-    void dfs(ArrayList<Integer>[] adj,int node,boolean[] visited){
-        visited[node]=true;
-        for(int x:adj[node]){
-            if(!visited[x]){
-                visited[x]=true;
-                dfs(adj,x,visited);
-            }
-        }
     }
 }
